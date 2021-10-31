c99df037ecba24f98e7bfedbdf46f354dfd52224
Tim Cocu <timcocu@gmail.com>
2021-10-31 16:14:45 +0100

    Add /health endpoint to verify service status

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+
+async fn health() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health", web::get().to(health))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
